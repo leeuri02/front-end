fce167dd1d7c7242662805280be62f4356855731
leeuri02 <lee_uri@naver.com>
2023-08-07 16:01:14 +0900

    login filex2

--- js_ex/practice3/code.js
+++ js_ex/practice3/code.js
@@ -4,16 +4,33 @@ let button1 = document.getElementById("change-text1");
 let button2 = document.getElementById("change-text2");
 let button3 = document.getElementById("change-text3");
 
-//ボタンをクリックすると初期値が変更する
-let count = 0
-button1.addEventListener("click",()=>{
-    label.innerHTML = count;
-});
+//値を文字列で定数にしておく
+const BASE_TEXT = "値："
 
+//計算結果を格納する変数
+let count = 0;
 
+//値を表示するメソッド
+function change(){
+    label.innerHTML = BASE_TEXT + count;
+}
+
+//値を計算するメソッド
+function calculate(setValue){
+    count = count + setValue;
+}
+
+//リセットボタンを押した時
 button1.addEventListener("click",()=>{
-    label.innerHTML = count++;
-});
-button1.addEventListener("click",()=>{
-    label.innerHTML = count--;
-});
+    count = 0;
+    change();
+})
+
+button2.addEventListener("click",()=>{
+    calculate(1);
+    change();
+})
+button3.addEventListener("click",()=>{
+    calculate(-1);
+    change();
+})
